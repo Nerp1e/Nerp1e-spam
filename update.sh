e1952ecf04edcb63209ba331fcf803392b59fcba
Денис Руденко <111921739+Nerp1e@users.noreply.github.com>
2023-09-22 22:25:45 +0300

    update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 cd
-rm -rf Spavveri
-git clone https://github.com/Hironotori/Spavveri
+rm -rf Nerp1e-spam
+git clone https://github.com/Nerp1e/Nerp1e-spam
 cd
-rm -rf Spavveri
-git clone https://github.com/Hironotori/Spavveri
+rm -rf Nerp1e-spam
+git clone https://github.com/Nerp1e/Nerp1e-spam
 cd
 cd Nerp1e
 python3 Nerp1e.py
